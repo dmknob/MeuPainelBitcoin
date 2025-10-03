c1a8e799126bde74093549ffae2eadcc0dee3dc3
Dênison Knob <48419+dmknob@users.noreply.github.com>
2025-10-03 19:54:23 -0300

    fix: PM2 logs usando diretorio padrao, com limite de memoria e renomeado para BitPanel

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,18 @@
 module.exports = {
   apps : [{
-    name   : "MeuPainelBitcoin", // O nome que já usamos para a aplicação
-    script : "server.js",      // O arquivo a ser executado
-
-    // Esta linha é importante: garante que o dotenv encontre o .env
-    cwd: __dirname,
+    name        : "BitPanel", // Nome da sua aplicação no PM2
+    script      : "server.js",
+    cwd         : __dirname,    // Mantém o diretório de trabalho correto
+    watch       : false,      // Geralmente 'false' em produção para evitar reinícios acidentais
+    exec_mode   : "fork",     // Confirmado como "fork"
+    instances   : 1,          // 1 instância para "fork"
+    env: {
+      "NODE_ENV": "production"
+    },
+    // --- Configurações de Otimização de Memória ---
+    max_memory_restart: "120M", // Reinicia se usar mais de 120MB de RAM (se o uso normal é 79.3M)
+    // --- Configurações de Log (Removidas para usar o padrão do PM2) ---
+    // O PM2 vai automaticamente salvar os logs em ~/.pm2/logs/BitPanel-out.log e BitPanel-error.log
+    time: true                      // Adiciona timestamp aos logs (ainda útil para logs padrão)
   }]
-}
+};
